a32f7f6334abac88339a637cc15aefbad47e040b
Betasa-MarkIan <betasa.gutierrezmarkian@gmail.com>
2025-01-13 09:56:38 +0800

    working on styles of header

---
+++ frontend/client/src/navigation/Header.jsx
@@ -0,0 +1,31 @@
+import style from './Header.module.css'
+
+function Header () {
+  return (
+    <>
+    <div className={style['header-box']}>
+      {/* for title */}
+      <div className={style['title-section']}>
+        <h1 className={style['title01']}>Workout</h1>
+        <h1 className={style['title02']}>planner</h1>
+      </div>
+
+      {/* for subsections in the header */}
+      <div className={style['sub-headings-section']}>
+      <div className={style['home-subheading']}>Home</div>
+      <div className={style['exercise-subheading']}>Exercise list</div>
+      <div className={style['workout-plans-subheading']}>Workout plans</div>
+      </div>
+
+      {/* for the account icon */}
+      <div className={style['account-section']}>
+        account
+      </div>
+    
+    </div>
+    </>
+    
+  )
+} 
+
+export default Header
